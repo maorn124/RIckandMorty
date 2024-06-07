34d3c348abf6cefb7519c1c98814ed1bb35a2104
maorn124 <maor.niv@gmail.com>
2024-06-07 16:11:47 -0400

    Initial Commit

---
+++ RIckandMorty/RIckandMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RIckandMortyApp.swift
+//  RIckandMorty
+//
+//  Created by Maor Niv on 6/7/24.
+//
+
+import SwiftUI
+
+@main
+struct RIckandMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
